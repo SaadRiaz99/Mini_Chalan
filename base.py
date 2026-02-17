5edf9716a0699a63ff07121df3f8b786bca72e97
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-17 18:21:31 +0500

    Add Some Code

--- base.py
+++ base.py
@@ -1,6 +1,18 @@
+import random 
+challan_pay = random.randint(250 ,1500)
 class vechile:
     def __init__(self, vech_name , speed):
         self.vech_name = vech_name
         self.speed = speed
+
+        def check_Speed(self):
+            if self.speed > 90 :
+                print(f"Your Speed is {self.speed} Km/h and Your Vechile Number is {self.vech_name} ")
+                self.challan()
+            else:
+                print(f"{self.name} is driving safely at {self.speed} km/h")
+
+        def challan():
+            print(f"Your Vechiel name is {self.vech_name}Speed\n{self.speed} \nChallan is PkR {challan_pay}")
 
         
