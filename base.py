5f63ee82d0e319940850a9b9eb0e611d0d55ac6d
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-17 18:36:02 +0500

    Solve ALl issue in code

--- base.py
+++ base.py
@@ -1,48 +1,60 @@
-import random 
-challan_pay = random.randint(250 ,1500)
+import random
+
+challan_pay = random.randint(250, 1500)
+
 class vechile:
-    def __init__(self, vech_name , speed):
+    def __init__(self, vech_name, speed):
         self.vech_name = vech_name
         self.speed = speed
 
-        def check_Speed(self):
-            if self.speed > 90 :
-                print(f"Your Speed is {self.speed} Km/h and Your Vechile Number is {self.vech_name} ")
-                self.challan()
-            else:
-                print(f"{self.name} is driving safely at {self.speed} km/h")
+    def check_Speed(self):
+        if self.speed > 90:
+            print(f"Your Speed is {self.speed} Km/h and Your Vechile Number is {self.vech_name}")
+            self.challan()
+        else:
+            print(f"{self.vech_name} is driving safely at {self.speed} km/h")
 
-        def challan():
-            print(f"Your Vechiel name is {self.vech_name}Speed\n{self.speed} \nChallan is PkR {challan_pay}")
-
+    def challan(self):
+        print(f"Your Vechile name is {self.vech_name}\nSpeed: {self.speed} \nChallan is PkR {challan_pay}")
 
 class car(vechile):
     def check_speed(self):
         if self.speed > 90:
-            print(f"Car {self.name} exceeded speed limit! Speed: {self.speed} km/h")
-            challan()
+            print(f"Car {self.vech_name} exceeded speed limit! Speed: {self.speed} km/h")
+            self.challan()
         else:
-            print(f"Car {self.name} is within speed limit.")
-
-
-
+            print(f"Car {self.vech_name} is within speed limit.")
 
 class Truck(vechile):
     def check_speed(self):
         if self.speed > 120:
-            print(f"Truck {self.name} exceeded speed limit! Speed: {self.speed} km/h")
-            challan()
+            print(f"Truck {self.vech_name} exceeded speed limit! Speed: {self.speed} km/h")
+            self.challan()
         else:
-            print(f"Truck {self.name} is within speed limit.")
-
-
+            print(f"Truck {self.vech_name} is within speed limit.")
 
 class Bike(vechile):
     def check_speed(self):
         if self.speed > 80:
-            print(f"Bike {self.name} exceeded speed limit! Speed: {self.speed} km/h")
-            challan()
+            print(f"Bike {self.vech_name} exceeded speed limit! Speed: {self.speed} km/h")
+            self.challan()
         else:
-            print(f"Bike {self.name} is within speed limit.")
+            print(f"Bike {self.vech_name} is within speed limit.")
 
+def Check_speed(vechile_obj):
+    vechile_obj.check_Speed()
 
+def main():
+    vehicles = [
+        car("Honda Civic", 100),
+        Bike("Yamaha R15", 85),
+        Truck("Volvo FH", 65),
+        car("Toyota Corolla", 80)
+    ]
+
+    print("---- Traffic Speed Check ----")
+    for v in vehicles:
+        Check_speed(v)
+
+if __name__ == "__main__":
+    main()
