70e2be4edb69919273ac73c594d6bfbd954693c1
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-18 16:16:25 +0500

    Add Vechile Registration Number

--- base.py
+++ base.py
@@ -3,13 +3,14 @@ import random
 challan_pay = random.randint(250, 1500)
 
 class vechile:
-    def __init__(self, vech_name, speed):
+    def __init__(self, vech_name, speed , rgs_number):
         self.vech_name = vech_name
+        self.rgs_number = rgs_number
         self.speed = speed
 
     def check_Speed(self):
         if self.speed > 90:
-            print(f"Your Speed is {self.speed} Km/h and Your Vechile Number is {self.vech_name}")
+            print(f"Your Speed is {self.speed} Km/h and Your Vechile Number is {self.vech_name} and Your Registration number is this {self.rgs_number}")
             self.challan()
         else:
             print(f"{self.vech_name} is driving safely at {self.speed} km/h")
